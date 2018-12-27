028b6279e494cfe07db312137999c48a5437d13d
Łukasz Jagodziński <luke.jagodzinski@gmail.com>
2018-12-27 12:17:16 +0100

    Bump version to 3.0.0

--- package.js
+++ package.js
@@ -1,22 +1,21 @@
 Package.describe({
-  name: 'jagi:astronomy-softremove-behavior',
-  version: '2.0.8',
-  summary: 'Soft remove behavior for Meteor Astronomy',
-  git: 'https://github.com/jagi/meteor-astronomy-softremove-behavior.git'
+  name: "jagi:astronomy-softremove-behavior",
+  version: "3.0.0",
+  summary: "Soft remove behavior for Meteor Astronomy",
+  git: "https://github.com/jagi/meteor-astronomy-softremove-behavior.git"
 });
 
 Npm.depends({
-  lodash: '4.17.2'
+  lodash: "4.17.11"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3');
+  api.versionsFrom("1.3");
 
-  api.use([
-    'ecmascript',
-    'es5-shim',
-    'jagi:astronomy@2.3.4'
-  ], ['client', 'server']);
+  api.use(
+    ["ecmascript", "es5-shim", "jagi:astronomy@2.3.4"],
+    ["client", "server"]
+  );
 
-  api.mainModule('lib/main.js', ['client', 'server'], { lazy: true });
+  api.mainModule("lib/main.js", ["client", "server"], { lazy: true });
 });
